59b701be2228789b2fb191f2e4712ad8b07229da
patrickcisco <priel@cisco.com>
2020-11-17 14:59:29 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import '@momentum-ui/core/css/momentum-ui.min.css';
+import '@webex/components/dist/css/webex-components.css';
+import React, {useEffect, useState} from 'react';
+import Webex from 'webex';
+import WebexSDKAdapter from '@webex/sdk-component-adapter';
+import {WebexAvatar, WebexDataProvider} from '@webex/components';
+
+const webex = new Webex({
+  credentials: '<CREDENTIALS>',
+});
+const adapter = new WebexSDKAdapter(webex);
 
 function App() {
+  const [adapterConnected, setAdapterConnected] = useState(false);
+
+  useEffect(() => {
+    // This is the suggested way to do async hooks.
+    // Ref: https://github.com/facebook/react/issues/14326
+    async function doConnect() {
+      // Wait for the adapter to connect before rendering.
+      await adapter.connect();
+      setAdapterConnected(true);
+    }
+
+    doConnect();
+
+    // On teardown, disconnect the adapter
+    return () => {
+      adapter.disconnect();
+    }
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        adapterConnected && (
+          <WebexDataProvider adapter={adapter} >
+            <WebexAvatar personID="<PERSON ID>" />
+          </WebexDataProvider>
+        )
+      }
     </div>
   );
 }
